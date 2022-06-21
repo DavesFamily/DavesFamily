ab9e3c3387fafed1204d38fea0ad738778a38c34
Rumen Manev <rumen.manev@mova.ie>
2022-06-21 22:05:16 +0300

    change nav button to link

--- packages/mars-theme/src/components/nav.js
+++ packages/mars-theme/src/components/nav.js
@@ -1,21 +1,12 @@
-import {
-  connect,
-  styled
-} from "frontity";
+import { connect, styled } from "frontity";
 import Link from "./link";
-import {
-  useEffect,
-  useState
-} from "react"
-import {
-  cookie,
-  useCookies
-} from "react-cookie";
+import { useEffect, useState } from "react";
+import { cookie, useCookies } from "react-cookie";
 
 import {
   connectWallet,
   getCurrentWalletConnected,
-  checkWhitelisted
+  checkWhitelisted,
 } from "../ethers/interact.js";
 
 /**
@@ -23,110 +14,120 @@ import {
  *
  * It renders the navigation links
  */
-const Nav = ({
-    state,
-    actions
-  }) => {
-    const currURL = state.router.link;
-    const items = state.source.get(`/menus/${state.theme.menuUrl}`).items;
+const Nav = ({ state, actions }) => {
+  const currURL = state.router.link;
+  const items = state.source.get(`/menus/${state.theme.menuUrl}`).items;
 
-    const [walletAddress, setWalletAddress] = useState("");
-    const [status, setStatus] = useState("");
-    // const [whitelisted, setWhiteListed] = useState(0)
-    const [cookies, setCookie] = useCookies(['iswalletconnected', 'iswhitelisted']);
+  const [walletAddress, setWalletAddress] = useState("");
+  const [status, setStatus] = useState("");
+  // const [whitelisted, setWhiteListed] = useState(0)
+  const [cookies, setCookie] = useCookies([
+    "iswalletconnected",
+    "iswhitelisted",
+  ]);
 
-    const connectWalletPressed = async (e) => {
-      // Call event for Connect Wallet
-      e.preventDefault()
-      // Comming walletResponse object {address, status}
-      const walletResponse = await connectWallet();
-      setWalletAddress(walletResponse.address);
+  const connectWalletPressed = async (e) => {
+    // Call event for Connect Wallet
+    e.preventDefault();
+    // Comming walletResponse object {address, status}
+    const walletResponse = await connectWallet();
+    setWalletAddress(walletResponse.address);
 
-      if(walletResponse.address) {
-        // Set iswalletconnected Cookie
-        setCookie('iswalletconnected', '1', {
-          path: '/'
-        });
-
-        // Check Is Whitelisted and Set iswhitelisted Cookie
-        const res = await checkWhitelisted();
-
-        (res) ? setCookie('iswhitelisted', '1', {
-          path: '/'
-        }): setCookie('iswhitelisted', '0', {
-          path: '/'
-        })
-
-        // Redirect to Mint Page
-        actions.router.set("/mint/"); 
-      }
-    };
-
-    useEffect(async () => {
-      const {
-        address,
-        status
-      } = await getCurrentWalletConnected();
-
-      setWalletAddress(address);
-      
-      addWalletListener();
-
+    if (walletResponse.address) {
       // Set iswalletconnected Cookie
-      if(address.length>0) {
-        setCookie('iswalletconnected', '1', {
-          path: '/'
-        })
-      }
+      setCookie("iswalletconnected", "1", {
+        path: "/",
+      });
 
-      /**
-       * Set cookies according to the results.
-       * If account is whitelisted, then `checkWhitelisted()` function
-       *  should return reserved amounts.
-       * If it is not whitelisted, then returns 0
-       */
+      // Check Is Whitelisted and Set iswhitelisted Cookie
       const res = await checkWhitelisted();
 
-      (res) ? setCookie('iswhitelisted', '1', {
-        path: '/'
-      }): setCookie('iswhitelisted', '0', {
-        path: '/'
-      })
+      res
+        ? setCookie("iswhitelisted", "1", {
+            path: "/",
+          })
+        : setCookie("iswhitelisted", "0", {
+            path: "/",
+          });
 
-      }, [])
+      // Redirect to Mint Page
+      actions.router.set("/mint/");
+    }
+  };
 
-    const addWalletListener = () => {
-      if (window.ethereum) {
-        window.ethereum.on("accountsChanged", (accounts) => {
-          if (accounts.length > 0) {
-            setWalletAddress(accounts[0]);
-            setStatus("Wallet connected");
-          } else {
-            setWalletAddress("");
-            setStatus("Wallet not connected");
-          }
-        });
-      } else {
-        setStatus("Make sure Metamask installed first");
-      }
+  useEffect(async () => {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWalletAddress(address);
+
+    addWalletListener();
+
+    // Set iswalletconnected Cookie
+    if (address.length > 0) {
+      setCookie("iswalletconnected", "1", {
+        path: "/",
+      });
     }
 
-  return(
+    /**
+     * Set cookies according to the results.
+     * If account is whitelisted, then `checkWhitelisted()` function
+     *  should return reserved amounts.
+     * If it is not whitelisted, then returns 0
+     */
+    const res = await checkWhitelisted();
+
+    res
+      ? setCookie("iswhitelisted", "1", {
+          path: "/",
+        })
+      : setCookie("iswhitelisted", "0", {
+          path: "/",
+        });
+  }, []);
+
+  const addWalletListener = () => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWalletAddress(accounts[0]);
+          setStatus("Wallet connected");
+        } else {
+          setWalletAddress("");
+          setStatus("Wallet not connected");
+        }
+      });
+    } else {
+      setStatus("Make sure Metamask installed first");
+    }
+  };
+
+  return (
     <ul className="header_menu_list">
-      {items.map((item)=>{
-        if((walletAddress.length > 0 && item.title==='Mint') || item.title!=='Mint') {
-          var menuURL = (currURL.includes('/react-home/')===false && item.url.includes("#"))?'/react-home/'+item.url:item.url;
-         return(
-          <li key={item.ID} className={(currURL === '/'+item.slug+'/') ? "active" : ""}>
-            <a href={menuURL}>
-            {item.title}
-            </a>
-          </li>
-         )
+      {items.map((item) => {
+        if (
+          (walletAddress.length > 0 && item.title === "Mint") ||
+          item.title !== "Mint"
+        ) {
+          var menuURL =
+            currURL.includes("/react-home/") === false && item.url.includes("#")
+              ? "/react-home/" + item.url
+              : item.url;
+          return (
+            <li
+              key={item.ID}
+              className={currURL === "/" + item.slug + "/" ? "active" : ""}
+            >
+              <a href={menuURL}>{item.title}</a>
+            </li>
+          );
         }
       })}
       <li key="custom">
-        <a onClick={connectWalletPressed}>{
+        <a href="https://mint.daves.family">
+          <span>Mint</span>
+        </a>
+        {/* <a onClick={connectWalletPressed}>{
         (walletAddress.length > 0) ? (
           "Connected:"+String(walletAddress).substring(0,6) + 
           "..." + 
@@ -134,10 +135,10 @@ const Nav = ({
         ):(
           <span>Connect Wallet</span>
           )
-        }</a>
+        }</a> */}
       </li>
     </ul>
-  )
+  );
 };
 
 export default connect(Nav);
